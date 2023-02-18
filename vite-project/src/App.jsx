1238116840c00b546ca68e08866f490805113117
Jack <li.jack0707@gmail.com>
2023-02-18 14:51:36 -0800

    chore: updated popup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,23 +4,23 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Map, { MapProvider, Marker, Popup } from "react-map-gl";
 import data from "./data.jsx";
-import Markers from "./Markers.jsx";
+import Markers from './Markers.jsx'
+import Sidebar from './Sidebar.jsx'
 
 export default function App() {
   const MAPBOX_TOKEN =
     "pk.eyJ1IjoiamFja2xpOTIxIiwiYSI6ImNsZTh4MXo1czAzY28zdnBhbGcyZ3Nqa2kifQ.XJu3d6iTqkELeKfFPBu3xQ";
-
+  
   const [viewState, setViewState] = useState({
     longitude: -79.34015,
     latitude: 43.65107,
     zoom: 11,
   });
 
-  const [selectedPlace, setSelectedPlace] = useState(null)
-
   return (
     <div>
       <h1>Places in Toronto</h1>
+      <Sidebar />
       <Map
         {...viewState}
         onMove={(evt) => setViewState(evt.viewState)}
@@ -29,30 +29,8 @@ export default function App() {
         mapboxAccessToken={MAPBOX_TOKEN}
         className="map"
       >
-        {data.map((place, index) => {
-          
-          const { long, lat, name } = place;
-          return (
-            <>
-              <Marker
-                key={`marker-${index}`}
-                longitude={long}
-                latitude={lat}
-                onClick={()=>{setSelectedPlace(place)}}
-              >
-              
-              </Marker>
-              {selectedPlace && (
-                <Popup
-                  longitude={selectedPlace.long}
-                  latitude={selectedPlace.lat}
-                >
-                  <p>{selectedPlace.name}</p>
-                </Popup>
-              )}
-            </>
-          );
-        })}
+        <Markers />
+
       </Map>
     </div>
   );
--- vite-project/src/Markers.jsx
+++ vite-project/src/Markers.jsx
@@ -1,12 +1,44 @@
+import React, { useState } from "react";
+import data from "./data.jsx";
+import mapboxgl from "mapbox-gl";
+import Map, { MapProvider, Marker, Popup } from "react-map-gl";
 
-import React from 'react'
-import data from './data.jsx'
-
-export default function Markers(){
-
-
-    
-    return (
-        <></>
-    )
+export default function Markers(props) {
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  
+  return (
+    <>
+      {data &&
+        data.length > 0 &&
+        data.map((place, index) => {
+          const { long, lat, name } = place;
+          return (
+            <React.Fragment key={index}>
+              <Marker
+                key={index}
+                longitude={long}
+                latitude={lat}
+                onClick={(e) => {
+                  e.originalEvent.stopPropagation();
+                  setSelectedPlace(place);
+                }}
+              ></Marker>
+              {selectedPlace && (
+                <Popup
+                  longitude={selectedPlace.long}
+                  latitude={selectedPlace.lat}
+                  onClose={() => {
+                    setSelectedPlace(null);
+                  }}
+                  offset={40}
+                >
+                  <p>{selectedPlace.name}</p>
+                </Popup>
+           
+              )}
+            </ React.Fragment>
+          );
+        })}
+    </>
+  );
 }
